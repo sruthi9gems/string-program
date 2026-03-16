208f53ee2b9bbf42bb2473f663bed87d4be7ad5f
Sruthi Krishna Murthi <sruthi.9gems@gmail.com>
2026-03-16 16:03:43 +0530

    initial commit

---
+++ One_Value_to_Multiple_Variables.py
@@ -0,0 +1,18 @@
+#One Value to Multiple Variables
+
+x = y = z = "Orange"  #rather than writing separately you can write orange once which is efficient
+
+print(x)
+print(y)
+print(z)
+
+
+#now separately
+a = "Orange"
+b = "Orange"
+c = "Orange"
+
+
+print(a)
+print(b)
+print(c)
