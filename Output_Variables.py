1857a22c24e894b6da5149cf39fc0620e5916b32
Sruthi Krishna Murthi <sruthi.9gems@gmail.com>
2026-03-17 14:31:45 +0530

    initial commit

---
+++ Output_Variables.py
@@ -0,0 +1,9 @@
+#Output Variables
+
+x = 5
+y = "John"
+print(x + y)#which will give an error 
+
+#so you can use ' , ' comma to add an int and str which can not be posible by + symbol where
+
+print(x , y)
