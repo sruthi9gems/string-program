5f38a1be136492b3db4bd1c6e1c16eadb32c6823
Sruthi Krishna Murthi <sruthi.9gems@gmail.com>
2026-03-18 08:37:05 +0530

    Add files via upload

---
+++ Unpack_a_Collection.py
@@ -0,0 +1,8 @@
+#Unpack a Collection
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits   #rather than writing separately this will be useful this is called unpacking
+
+print(x)
+print(y)
+print(z)
